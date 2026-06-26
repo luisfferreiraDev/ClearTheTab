1160161a4e0dec194b4b1ed8cbc81d3955998aef
luisfferreiraDev <luisfferreira.dev@gmail.com>
2026-06-26 16:14:13 +0100

    chore: update Svelte adapter to Cloudflare and adjust app types
    - Changed adapter from @sveltejs/adapter-auto to @sveltejs/adapter-cloudflare in package.json and vite.config.ts.
    - Enhanced app.d.ts by defining the Platform interface with env, cf, and ctx properties.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -10,14 +10,9 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		}),
 		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' }),
